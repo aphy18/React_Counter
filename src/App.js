c404131c008962effeef2759158b261307af7ef9
aphy18 <aphason1@gmail.com>
2022-11-01 15:19:57 -0400

    bug fix to state

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import styles from './styles.css';
 function App() {
 
   let [counter, setCounter] = useState(0);
+  let [buttonText, setButtonText] = useState('Add to Counter');
+  let [toggle, setToggle] = useState(true);
+
   let bottomMain;
   let sumContainer;
   let textfield;
   let label;
   let toggleText;
   let enterNum;
-  let toggledOn = false;
+
 
 
   function toggleCounter() {
-    toggledOn = !toggledOn;
-    console.log('toggled on', toggledOn)
+
+    setToggle(!toggle);
+    console.log('toggle', toggle);
     bottomMain = document.querySelector('.bottom-main');
     toggleText = document.querySelector('.change');
 
@@ -24,7 +28,7 @@ function App() {
     label = document.createElement('label');
     enterNum = document.createElement('button');
   
-    if (toggledOn === true) {
+    if (toggle === true) {
       textfield.type = 'text';
       label.innerText = 'Enter Number to sum with counter: '
       enterNum.innerText = 'Add';
@@ -33,12 +37,13 @@ function App() {
       sumContainer.append(label, textfield, enterNum);
       bottomMain.append(sumContainer);
       enterNum.addEventListener('click', updateCounter)
+      setButtonText('Escape')
     } else {
       while (bottomMain.firstChild) {
         bottomMain.removeChild(bottomMain.lastChild)
+        setButtonText('Add to counter')
       }
     }
-
   }
   
   function updateCounter() {
@@ -59,7 +64,7 @@ function App() {
         <button className="increment" onClick={() => setCounter(counter + 1)}>Count Up</button>
         <button className="decrement" onClick={() => setCounter(counter > 0 ? counter - 1 : 0)}>Count Down</button>
         <button className="reset" onClick={() => setCounter(0)}>Reset</button>
-        <button className="change" onClick={() => toggleCounter()}>Add to Counter</button>
+        <button className="change" onClick={() => toggleCounter()}>{buttonText}</button>
       </div>
       <div className="bottom-main"></div>
     </main>
