df9991dacb09a685ef13a5e29b28b5f9fc7698c6
aphy18 <aphason1@gmail.com>
2022-11-02 13:15:04 -0400

    calculate basic arithmetic with counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import { React, useState, useRef } from 'react';
-import styles from './styles.css';
+// import './../styles/styles.css'
+
 
 function App() {
 
   let [counter, setCounter] = useState(0);
-  let [buttonText, setButtonText] = useState('Add to Counter');
+  let [buttonText, setButtonText] = useState('Operation');
   let [toggle, setToggle] = useState(true);
 
-  let bottomMain;
+  let bottomMain 
   let sumContainer;
   let textfield;
   let label;
   let toggleText;
   let enterNum;
 
-
-
   function toggleCounter() {
 
+    console.log('COUNTER -->', counter)
     setToggle(!toggle);
     console.log('toggle', toggle);
     bottomMain = document.querySelector('.bottom-main');
@@ -27,11 +27,13 @@ function App() {
     textfield = document.createElement('input');
     label = document.createElement('label');
     enterNum = document.createElement('button');
+    
+
   
-    if (toggle === true) {
+    if (toggle) {
       textfield.type = 'text';
-      label.innerText = 'Enter Number to sum with counter: '
-      enterNum.innerText = 'Add';
+      label.innerText = 'Enter Number: '
+      enterNum.innerText = 'Calculate';
       enterNum.classList.add('enterNum');
       sumContainer.classList.add('block');
       sumContainer.append(label, textfield, enterNum);
@@ -41,16 +43,34 @@ function App() {
     } else {
       while (bottomMain.firstChild) {
         bottomMain.removeChild(bottomMain.lastChild)
-        setButtonText('Add to counter')
+        setButtonText('Operation')
       }
     }
   }
   
   function updateCounter() {
     let value = parseInt(textfield.value);
-    if (!isNaN(value))
-    setCounter(counter + value);
-    textfield.value = '';
+    if (!isNaN(value)) {
+      let operation = prompt('Enter operation (+, -, *, /)')
+      switch(operation) {
+        case '+':
+          setCounter(counter += value);
+          console.log('counter', counter)
+          break;
+        case '-':
+          setCounter(counter -= value);
+          break;
+        case '*':
+          setCounter(counter *= value);
+          break;
+        case '/':
+          setCounter(counter /= value);
+          break;
+        default:
+          alert('not an operation')
+      }
+    }
+    
   }
 
 
@@ -61,8 +81,8 @@ function App() {
 
     <main className="main">
       <div className="top-main">
-        <button className="increment" onClick={() => setCounter(counter + 1)}>Count Up</button>
-        <button className="decrement" onClick={() => setCounter(counter > 0 ? counter - 1 : 0)}>Count Down</button>
+        <button className="increment" onClick={() => setCounter(counter = counter + 1)}>Count Up</button>
+        <button className="decrement" onClick={() => setCounter(counter > 0 ? counter = counter - 1 : 0)}>Count Down</button>
         <button className="reset" onClick={() => setCounter(0)}>Reset</button>
         <button className="change" onClick={() => toggleCounter()}>{buttonText}</button>
       </div>
